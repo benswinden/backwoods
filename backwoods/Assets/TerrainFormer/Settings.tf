8821bf3ba933d1e0e2f23458a4f83124b1724094
ben <swinden.ben@gmail.com>
2017-02-13 21:26:42 -0500

    : unity standard image effects

---
+++ backwoods/Assets/TerrainFormer/Settings.tf
@@ -0,0 +1,233 @@
+{
+	"@type": "JesseStiller.TerrainFormerExtension.Settings",
+	"modeSettings": {
+		"RaiseOrLower": {
+			"@type": "JesseStiller.TerrainFormerExtension.ModeSettings",
+			"useFalloffForCustomBrushes": false,
+			"useAlphaFalloff": false,
+			"selectedBrushTab": "All",
+			"selectedBrushId": "_DefaultProceduralBrush",
+			"brushSize": 45,
+			"brushSpeed": 0.05,
+			"brushRoundness": 1,
+			"brushAngle": 0,
+			"brushFalloffFauxFrames": [
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 0,
+					"tangentMode": 0,
+					"time": 0,
+					"value": 0
+				},
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 1,
+					"tangentMode": 0,
+					"time": 1,
+					"value": 1
+				}
+			],
+			"useBrushSpacing": false,
+			"useRandomRotation": false,
+			"useRandomOffset": false,
+			"invertBrushTexture": false,
+			"randomOffset": 10,
+			"MinBrushSpacing": 1,
+			"MaxBrushSpacing": 30,
+			"MinRandomRotation": -180,
+			"MaxRandomRotation": 180
+		},
+		"Smooth": {
+			"@type": "JesseStiller.TerrainFormerExtension.ModeSettings",
+			"useFalloffForCustomBrushes": false,
+			"useAlphaFalloff": false,
+			"selectedBrushTab": "All",
+			"selectedBrushId": "_DefaultProceduralBrush",
+			"brushSize": 80,
+			"brushSpeed": 1,
+			"brushRoundness": 1,
+			"brushAngle": 0,
+			"brushFalloffFauxFrames": [
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 0,
+					"tangentMode": 0,
+					"time": 0,
+					"value": 0
+				},
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 1,
+					"tangentMode": 0,
+					"time": 1,
+					"value": 1
+				}
+			],
+			"useBrushSpacing": false,
+			"useRandomRotation": false,
+			"useRandomOffset": false,
+			"invertBrushTexture": false,
+			"randomOffset": 10,
+			"MinBrushSpacing": 1,
+			"MaxBrushSpacing": 30,
+			"MinRandomRotation": -180,
+			"MaxRandomRotation": 180
+		},
+		"SetHeight": {
+			"@type": "JesseStiller.TerrainFormerExtension.ModeSettings",
+			"useFalloffForCustomBrushes": false,
+			"useAlphaFalloff": false,
+			"selectedBrushTab": "All",
+			"selectedBrushId": "_DefaultProceduralBrush",
+			"brushSize": 35,
+			"brushSpeed": 2,
+			"brushRoundness": 1,
+			"brushAngle": 0,
+			"brushFalloffFauxFrames": [
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 0,
+					"tangentMode": 0,
+					"time": 0,
+					"value": 0
+				},
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 1,
+					"tangentMode": 0,
+					"time": 1,
+					"value": 1
+				}
+			],
+			"useBrushSpacing": false,
+			"useRandomRotation": false,
+			"useRandomOffset": false,
+			"invertBrushTexture": false,
+			"randomOffset": 10,
+			"MinBrushSpacing": 1,
+			"MaxBrushSpacing": 30,
+			"MinRandomRotation": -180,
+			"MaxRandomRotation": 180
+		},
+		"Flatten": {
+			"@type": "JesseStiller.TerrainFormerExtension.ModeSettings",
+			"useFalloffForCustomBrushes": false,
+			"useAlphaFalloff": false,
+			"selectedBrushTab": "All",
+			"selectedBrushId": null,
+			"brushSize": 35,
+			"brushSpeed": 2,
+			"brushRoundness": 1,
+			"brushAngle": 0,
+			"brushFalloffFauxFrames": [
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 0,
+					"tangentMode": 0,
+					"time": 0,
+					"value": 0
+				},
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 1,
+					"tangentMode": 0,
+					"time": 1,
+					"value": 1
+				}
+			],
+			"useBrushSpacing": false,
+			"useRandomRotation": false,
+			"useRandomOffset": false,
+			"invertBrushTexture": false,
+			"randomOffset": 10,
+			"MinBrushSpacing": 1,
+			"MaxBrushSpacing": 50,
+			"MinRandomRotation": -180,
+			"MaxRandomRotation": 180
+		},
+		"PaintTexture": {
+			"@type": "JesseStiller.TerrainFormerExtension.ModeSettings",
+			"useFalloffForCustomBrushes": false,
+			"useAlphaFalloff": false,
+			"selectedBrushTab": "All",
+			"selectedBrushId": null,
+			"brushSize": 35,
+			"brushSpeed": 1,
+			"brushRoundness": 1,
+			"brushAngle": 0,
+			"brushFalloffFauxFrames": [
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 0,
+					"tangentMode": 0,
+					"time": 0,
+					"value": 0
+				},
+				{
+					"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+					"inTangent": 0,
+					"outTangent": 1,
+					"tangentMode": 0,
+					"time": 1,
+					"value": 1
+				}
+			],
+			"useBrushSpacing": false,
+			"useRandomRotation": false,
+			"useRandomOffset": false,
+			"invertBrushTexture": false,
+			"randomOffset": 10,
+			"MinBrushSpacing": 1,
+			"MaxBrushSpacing": 50,
+			"MinRandomRotation": -180,
+			"MaxRandomRotation": 180
+		}
+	},
+	"generateRampCurveFaux": [
+		{
+			"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+			"inTangent": 0,
+			"outTangent": 0,
+			"tangentMode": 0,
+			"time": 0,
+			"value": 0
+		},
+		{
+			"@type": "JesseStiller.TerrainFormerExtension.FauxKeyframe",
+			"inTangent": 0,
+			"outTangent": 1,
+			"tangentMode": 0,
+			"time": 1,
+			"value": 1
+		}
+	],
+	"generateHeight": 5,
+	"flattenMode": "Flatten",
+	"setHeight": 50,
+	"boxFilterSize": 3,
+	"smoothingIterations": 1,
+	"mainDirectory": "Assets/TerrainFormer/",
+	"showSculptingGridPlane": false,
+	"raycastOntoFlatPlane": true,
+	"showSceneViewInformation": true,
+	"displaySculptOntoMode": true,
+	"displayCurrentTool": true,
+	"displayCurrentHeight": true,
+	"displayBrushSizeIncrement": true,
+	"brushSizeIncrementMultiplier": 0.005,
+	"brushPreviewSize": 48,
+	"texurePreviewSize": 64,
+	"alwaysShowBrushSelection": false,
+	"alwaysUpdateTerrainLODs": true,
+	"invertBrushTexturesGlobally": false,
+	"brushSelectionDisplayType": "Tabbed"
+}
