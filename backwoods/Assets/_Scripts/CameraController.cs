ad1235a5b26b7a7db9addf906312c5eb7b72c36c
ben <swinden.ben@gmail.com>
2017-02-24 01:20:06 -0500

    stuuffff : [

--- backwoods/Assets/_Scripts/CameraController.cs
+++ backwoods/Assets/_Scripts/CameraController.cs
@@ -5,9 +5,51 @@ public class CameraController : MonoBehaviour {
 
     public float moveSpeed;
 
+    public Camera actualCamera;
+
+    [Space]
+    public bool animating;
+    [Header("Vignette")]
+    [Range(0,1)]
+    public float v_intensity;
+    [Range(0, 1)]
+    public float v_smoothness;
+
+    float fastSpeed = 1f;
+    float currentSpeed;
+
+
+
+    void Awake() {
+
+        currentSpeed = moveSpeed;
+
+        transform.position = new Vector3(transform.position.x, 0, 0);        
+    }
+   
     void Update() {
 
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed);
+        // Camera Movement
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + currentSpeed);
 
+        // Animation
+        if (animating) {
+
+            Manager.postController.vignette.intensity = v_intensity;
+            Manager.postController.vignette.smoothness = v_smoothness;
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+
+            if (currentSpeed == moveSpeed)
+                currentSpeed = fastSpeed;
+            else
+                currentSpeed = moveSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.Q)) {
+
+            GetComponent<Animator>().SetTrigger("FadeToBlack");
+        }
     }
 }
